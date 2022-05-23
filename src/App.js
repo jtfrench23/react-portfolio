e852a06e7e5134598abf6978296ad5df471749d5
jtfrench23 <92399356+jtfrench23@users.noreply.github.com>
2022-05-22 19:11:15 -0500

    added assets and made first component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+// import Home from './components/Home'
+// import About from './components/About'
+// import Contact from './components/Contact'
+import Layout from './components/Layouts'
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+      </Routes>
+    </>
+  )}
 export default App;
