d8cd08e26bc49fc094529ef365ebc908e76563d7
jtfrench23 <92399356+jtfrench23@users.noreply.github.com>
2022-05-22 20:27:12 -0500

    sidebar complete

--- src/components/Layouts/index.js
+++ src/components/Layouts/index.js
@@ -1,7 +1,11 @@
+// import Sidebar from '../Sidebar';
+
 import './index.scss';
 
+import Sidebar from '../Sidebar';
+
 const Layout = () => {
-    return <>Hello</>
+    return <Sidebar />
 }
 
 export default Layout;
