0812fb406ad7f4201b10b07891de08232708306c
jtfrench23 <92399356+jtfrench23@users.noreply.github.com>
2022-05-28 09:40:02 -0500

    added two more project boxes with overflow scroll

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -20,6 +20,14 @@ const Projects = () => {
             <h2>OnTrack</h2>
             <p>This app is designed for track coaches to keep track of athletes' times in track events as well as post messages/announcements for their team to see.  Athletes can view all of their previous times as well as their teammates' top times and their team's records.  This was a group project where my main role was backend development.  I created the database, constructed many of the sql queries, tested the application for bugs, and made sure data was being passed and displayed properly between the application and the database.</p>
         </div>
+        <div className='projectBox'>
+            <h2>French's Boutique</h2>
+            <p>This app is designed for track coaches to keep track of athletes' times in track events as well as post messages/announcements for their team to see.  Athletes can view all of their previous times as well as their teammates' top times and their team's records.  This was a group project where my main role was backend development.  I created the database, constructed many of the sql queries, tested the application for bugs, and made sure data was being passed and displayed properly between the application and the database.</p>
+        </div>
+        <div className='projectBox'>
+            <h2>Sasquatch Sitings</h2>
+            <p>This app is designed for track coaches to keep track of athletes' times in track events as well as post messages/announcements for their team to see.  Athletes can view all of their previous times as well as their teammates' top times and their team's records.  This was a group project where my main role was backend development.  I created the database, constructed many of the sql queries, tested the application for bugs, and made sure data was being passed and displayed properly between the application and the database.</p>
+        </div>
     </div>
     <Loader type='pacman'/>
     </>
